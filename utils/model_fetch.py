755b02453a02982ee621674e1232f734ae151c93
Deepan Das <ddas@scortex.io>
2020-04-02 23:12:04 -0400

    Pre activation Resnet Model added

--- utils/model_fetch.py
+++ utils/model_fetch.py
@@ -1,25 +1,27 @@
 from models import resnet_model
+from models import preact_resnet_model
 
-def fetch_teacher(model_name):
-    if model_name == 'resnet34':
-        return resnet_model.ResNet34()
-    elif model_name == 'resnet50':
-        return resnet_model.ResNet50()
-    elif model_name == 'resnet101':
-        return resnet_model.ResNet101()
-    elif model_name == 'resnet152':
-        return resnet_model.ResNet152()
-    else:
-        raise ValueError("Not a valid model name")
+_MODEL_DICT = {
+    'resnet18': resnet_model.ResNet18(),
+    'resnet34': resnet_model.ResNet34(),
+    'resnet50': resnet_model.ResNet50(),
+    'resnet101': resnet_model.ResNet101(),
+    'resnet152': resnet_model.ResNet152(),
+    'preact_resnet18': preact_resnet_model.PreActResNet18(),
+    'preact_resnet34': preact_resnet_model.PreActResNet34(),
+    'preact_resnet50': preact_resnet_model.PreActResNet50(),
+    'preact_resnet101': preact_resnet_model.PreActResNet101(),
+    'preact_resnet152': preact_resnet_model.PreActResNet152(),
+}
 
-def fetch_student(model_name):
-    if model_name == 'resnet18':
-        return resnet_model.ResNet18()
-    elif model_name == 'resnet50':
-        return resnet_model.ResNet50()
-    elif model_name == 'resnet101':
-        return resnet_model.ResNet101()
-    elif model_name == 'resnet34':
-        return resnet_model.ResNet34()
-    else:
-        raise ValueError("Not a valid model name")
+
+def _invalid_model_name():
+    raise ValueError("Not a valid model name")
+
+
+def fetch_teacher(model_name, model_dicts=_MODEL_DICT):
+    return model_dicts[model_name]
+
+
+def fetch_student(model_name, model_dicts=_MODEL_DICT):
+    return model_dicts[model_name]
