0fc23f769e72ab4e5a00311751201a2d0f20a7e7
Deepan Das <ddas@scortex.io>
2020-04-23 04:48:27 -0400

    Adding dataloader for numpy type datasets

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,17 +1,24 @@
 import numpy as np
 import torch
 import torchvision
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from PIL import Image
 
+
+_TRANSFORM = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
 
 def fetch_dataloader(mode, transform, batch_size=16):
 
     train_set = torchvision.datasets.CIFAR10(
-        root='./data-cifar-10', train=True, download=True, transform=transform)
+        root='./data/data-cifar-10', train=True, download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(
         train_set, batch_size=batch_size, shuffle=True)
 
     test_set = torchvision.datasets.CIFAR10(
-        root='./data-cifar-10', train=False, download=True, transform=transform)
+        root='./data/data-cifar-10', train=False, download=True, transform=transform)
     test_loader = torch.utils.data.DataLoader(
         test_set, batch_size=batch_size, shuffle=False)
 
@@ -19,3 +26,22 @@ def fetch_dataloader(mode, transform, batch_size=16):
         return train_loader
     else:
         return test_loader
+
+
+class MyDataset(Dataset):
+    def __init__(self, data, targets, transform=_TRANSFORM):
+        self.data = data #.transpose((0,2,3,1))
+        self.targets = torch.Tensor(targets)
+        self.transform = transform
+    
+    def __getitem__(self, index):
+        y = self.targets[index]
+        x = self.data[index]        
+        if self.transform:
+            x = Image.fromarray(np.uint8(x))
+            x = self.transform(x)
+        
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
